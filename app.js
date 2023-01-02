b6002f437220e9c257faaca466f332ed6d28f4be
Fábry Zoltán <tmntt2@gmail.com>
2023-01-02 17:55:27 +0100

    A feladat megoldásához szükséges file-ok létrehozva

---
+++ app.js
@@ -0,0 +1,5 @@
+let minus = document.getElementById("minus");
+let reset = document.getElementById("reset");
+let plus = document.getElementById("plus");
+let value = document.getElementById("value").value;
+
